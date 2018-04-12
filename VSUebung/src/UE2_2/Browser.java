f7b27500dd3415b24aa222bac275bc13d86489d0
StefanRauchbauer <38309729+StefanRauchbauer@users.noreply.github.com>
2018-04-12 14:25:19 +0200

    Uebung 2 grtzig

--- VSUebung/src/UE2_2/Browser.java
+++ VSUebung/src/UE2_2/Browser.java
@@ -1,6 +1,7 @@
 package UE2_2;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
@@ -16,45 +17,46 @@ import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 
 public class Browser extends JFrame implements ActionListener{
-	
+
 	private int downloads;
 	private JProgressBar[] balken;
 	private JButton startButton;
 	private CyclicBarrier barrier;
-
+	CountDownLatch countDownLatch;
 	// Deklaration Ihrer Synchronisations-Hilfsklassen hier:
 
 
 
 
-	
+
 	public Browser(int downloads ) {
 		super("Mein Download-Browser");
 		this.downloads = downloads;
-		barrier = new CyclicBarrier(downloads);
-		
-		
+		barrier = new CyclicBarrier(downloads+1);
+		countDownLatch = new CountDownLatch(downloads);
+
 		// Initialisierung Ihrer Synchronisations-Hilfsklassen hier:
 
 
-		
 
 
 
-		// Aufbau der GUI-Elemente:	
+
+		// Aufbau der GUI-Elemente:
 		balken = new JProgressBar[downloads];
 		JPanel zeilen = new JPanel(new GridLayout(downloads, 1));
-		
+
 		for(int i=0; i<downloads; i++) {
-			JPanel reihe = new JPanel(new FlowLayout(FlowLayout.LEADING, 0, 10));
+			JPanel reihe = new JPanel(new FlowLayout(FlowLayout.TRAILING, 0, 10));
 			balken[i] = new JProgressBar(0, 100);
                         balken[i].setPreferredSize(new Dimension(500, 20));
+                        balken[i].setBackground(new Color(200,0,0));
 			reihe.add(balken[i]);
 			zeilen.add(reihe);
 
 
-			 new Download(balken[i],barrier).start();
-			
+			 new Download(balken[i],barrier,countDownLatch).start();
+
 			// neue Download-Threads erzeugen und starten
 			// ggf. müssen Synchronisations-Objekte im Konstruktor übergeben werden!!
 			// balken ist ebenfalls zu übergeben!
@@ -65,18 +67,18 @@ public class Browser extends JFrame implements ActionListener{
 
 		startButton = new JButton("Downloads starten");
 		startButton.addActionListener(this);
-		
+
 		this.add(zeilen, BorderLayout.CENTER);
 		this.add(startButton, BorderLayout.SOUTH);
-		
-		
+
+
 		pack();
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
-	
+
 	public static void main(String[] args) throws InterruptedException {
-		new Browser(7);
+		new Browser(5);
 	}
 
 	@Override
@@ -91,7 +93,32 @@ public class Browser extends JFrame implements ActionListener{
 		startButton.setEnabled(false);
 		startButton.setSelected(false);
 		startButton.setText("Downloads laufen...");
-		
+
+		try {
+			barrier.await();
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (BrokenBarrierException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		new Thread(new Runnable()
+		{
+			public void run()
+			{
+				try {
+					countDownLatch.await();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				startButton.setText("Ende");
+			}
+
+		}).start();
+
 		// Auf Ende aller Download-Threads warten ... erst dann die Beschriftung ändern
 		// Achtung, damit die Oberflaeche "reaktiv" bleibt dies in einem eigenen Runnable ausfuehren!
 
--- VSUebung/src/UE2_2/Download.java
+++ VSUebung/src/UE2_2/Download.java
@@ -3,6 +3,7 @@ package UE2_2;
 
 import java.util.Random;
 import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.CyclicBarrier;
 
 import javax.swing.JProgressBar;
@@ -10,17 +11,19 @@ import javax.swing.JProgressBar;
 // aktive Klasse
 public class Download extends Thread
 {
-	
+
 	private final JProgressBar balken;
 	private int n;
 	private CyclicBarrier barrier1 = null;
+	private CountDownLatch countDownLatch;
+
     // weitere Attribute zur Synchronisation hier definieren
-    
-	public Download(JProgressBar balken,CyclicBarrier barrier1 /* ggf. weitere Objekte */) {
+
+	public Download(JProgressBar balken,CyclicBarrier barrier1, CountDownLatch cdl /* ggf. weitere Objekte */) {
 		this.balken = balken;
         // ...
 		this.barrier1 = barrier1;
-		
+		this.countDownLatch = cdl;
     }
 
 
@@ -29,12 +32,14 @@ public class Download extends Thread
      *  Mit balken.setValue(...) kann man den Balken einstellen (wieviel gefuellt) dargestellt wird
      *  Setzen Sie den value jeweils und legen Sie die Methode dann für eine zufaellige Zeit schlafen
      */
-		
+
 	@Override
 	public void run()
 	{
 		try {
+			System.out.println(this.currentThread().getName()+ " wartet");
 			this.barrier1.await();
+			System.out.println(this.currentThread().getName()+ " startet");
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -42,20 +47,22 @@ public class Download extends Thread
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		while(balken.getMaximum()!=n)
 		{
 		n++;
 		balken.setValue(n);
 		try {
-			Thread.sleep(new	Random().nextInt(1000));
+			Thread.sleep(new	Random().nextInt(100));
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
 		}
+		countDownLatch.countDown();
 	}
-	
+
 
 
 }
